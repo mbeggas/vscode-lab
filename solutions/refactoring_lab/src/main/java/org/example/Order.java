364e2d8743961c27493d099e69ca1629d378f639
drLacheheb <yacinolach@gmail.com>
2025-03-09 09:47:33 +0100

    Add refactoring lab solution

---
+++ solutions/refactoring_lab/src/main/java/org/example/Order.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.List;
+
+class Order {
+    private Customer customer;
+    private List<Item> items;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+}
+
