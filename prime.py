632ed83f71c25a89bde9a647aa44780a2e9b2277
chiraz <chirazkahla9@gmail.com>
2025-04-15 13:37:50 +0100

    First commit

---
+++ prime.py
@@ -0,0 +1,13 @@
+import math
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:  
+        return True
+    if n % 2 == 0:  
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):  
+        if n % i == 0:
+            return False
+    return True
