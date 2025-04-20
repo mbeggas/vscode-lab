b0af5dc877f4190edf89912b8e881e8266ec2a35
aliba39 <ezela809@gmail.com>
2025-04-20 18:52:07 +0100

    After Refactoring

--- app/src/main/java/org/example/Calculator.java
+++ app/src/main/java/org/example/Calculator.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Calculator {
-    public double calc(double a, double b) {
+    public double calculateSumDividedByProduct(double a, double b) {
         double x = a + b;
         double y = a * b;
         return x / y;
