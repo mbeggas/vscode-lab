2a03646d132b4d394e23adc17cddef8f9e6b2464
tidjanileghrissi <leghrissit@gmail.com>
2025-04-20 19:02:38 +0100

    before refactoring

---
+++ src/main/java/com/example/Order.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.util.List;
+
+public class Order {
+
+    private Customer customer;
+    private List<Item> items;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+}
