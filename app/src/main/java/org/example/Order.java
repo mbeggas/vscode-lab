f641579e3e9a479c6f1b653a0ee5c6fc6b767483
wijden04 <widjdenmahcen@gmail.com>
2025-04-26 17:24:55 +0100

    Tp before refactoring

---
+++ app/src/main/java/org/example/Order.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.List;
+
+public class Order {
+    private Customer customer;
+    private List<Item> items;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+}
