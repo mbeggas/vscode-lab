0b7d7a44d85d51844b0a72d75fd65aee333b20c2
yasmeeeeeen <yasmeeeeeen>
2025-04-26 22:06:14 -0800

    tp_before_refactor

---
+++ app/src/main/java/Customer.java
@@ -0,0 +1,12 @@
+class Customer {
+    private String name;
+    private boolean isMember;
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isMember() {
+        return isMember;
+    }
+}
---
+++ app/src/main/java/order.java
@@ -0,0 +1,12 @@
+class Order {
+    private Customer customer;
+    private List<Item> items;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+}
