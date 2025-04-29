299f0cc5cfc53ddbaae5cb6aea1054863f1deaaf
Radebr <radebr@yandex.ru>
2025-04-30 00:21:07 +0100

    fix: update App.java

--- refactoring_lab/app/src/main/java/org/example/App.java
+++ refactoring_lab/app/src/main/java/org/example/App.java
@@ -5,18 +5,15 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        // 1. أنشئ زبون (عضو)
-        Customer customer = new Customer("Ali", true);
 
-        // 2. أنشئ بعض المنتجات
+        Customer customer = new Customer("Rade", true);
+
         Item item1 = new Item("Book", 10.0, 2);
         Item item2 = new Item("Pen", 2.5, 4);
         List<Item> items = Arrays.asList(item1, item2);
 
-        // 3. أنشئ الطلب
         Order order = new Order(customer, items);
 
-        // 4. استخدم OrderProcessor لطباعة الملخص
         OrderProcessor processor = new OrderProcessor();
         processor.printOrderSummary(order);
     }
