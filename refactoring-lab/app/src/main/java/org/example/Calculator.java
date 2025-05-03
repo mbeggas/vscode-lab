5755d725138008f5fdbba6fbb2c55938a5cbd713
haahmed01 <dzahme33@gmail.com>
2025-05-03 23:29:01 +0100

    refactoring after

--- refactoring-lab/app/src/main/java/org/example/Calculator.java
+++ refactoring-lab/app/src/main/java/org/example/Calculator.java
@@ -1,13 +1,13 @@
 package org.example;
 
 public class Calculator {
-    public double calc(double a, double b) {
-        double x = a + b;
-        double y = a * b;
-        return x / y;
+    public double calculateRatio(double a, double b) {
+        double sum = a + b;
+        double product = a * b;
+        return sum / product;
     }
 
-    public void prtRes(double res) {
-        System.out.println("Result: " + res);
+    public void printResult(double result) {
+        System.out.println("Result: " + result);
     }
 }
