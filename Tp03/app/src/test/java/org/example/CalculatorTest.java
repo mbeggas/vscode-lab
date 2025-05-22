4e72319470ba30a697c82850e2b6397f4d62cde2
Bachir Dridi <Doumoua387@gmail.com>
2025-05-22 02:26:37 +0100

    refactor(tp03): rename Calculator methods and variables for clarity

--- Tp03/app/src/test/java/org/example/CalculatorTest.java
+++ Tp03/app/src/test/java/org/example/CalculatorTest.java
@@ -7,8 +7,8 @@ public class CalculatorTest {
     Calculator calc = new Calculator();
 
     @Test
-    public void testCalc() {
-        double result = calc.calc(4, 2);
+    public void testCalculateSumDividedByProduct() {
+        double result = calc.calculateSumDividedByProduct(4, 2);
         assertEquals(6.0 / 8.0, result, 0.0001);
     }
 }
